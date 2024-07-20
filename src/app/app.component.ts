8545c2afe1bee70257520ade0732690fa479b805
Nícolas Aigner <nicolas.aigner@gmail.com>
2024-07-20 03:23:00 -0300

    Adição do Cinto de Cartas de forma estática

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,115 +13,41 @@ import {MatGridList, MatGridTile} from "@angular/material/grid-list";
 import {MatFormField} from "@angular/material/form-field";
 import {MatOption, MatSelect} from "@angular/material/select";
 import {animate, state, style, transition, trigger} from "@angular/animations";
-import {MatTableModule} from "@angular/material/table";
+import {MatTableDataSource, MatTableModule} from "@angular/material/table";
 import {map, startWith} from "rxjs/operators";
 import {RequestApiService} from "./services/request-api.service";
 import {MatDivider} from "@angular/material/divider";
+import {MatInput} from "@angular/material/input";
 
-export interface ExampleTab {
-  label: string;
-  content: string;
+// Definindo a interface para os dados da tabela
+export interface MonsterGroup {
+  set: string;
+  raceAndSize: string;
+  monsters: string;
 }
 
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-  description: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {
-    position: 1,
-    name: 'Hydrogen',
-    weight: 1.0079,
-    symbol: 'H',
-    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`,
-  },
-  {
-    position: 2,
-    name: 'Helium',
-    weight: 4.0026,
-    symbol: 'He',
-    description: `Helium is a chemical element with symbol He and atomic number 2. It is a
-        colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
-        group in the periodic table. Its boiling point is the lowest among all the elements.`,
-  },
-  {
-    position: 3,
-    name: 'Lithium',
-    weight: 6.941,
-    symbol: 'Li',
-    description: `Lithium is a chemical element with symbol Li and atomic number 3. It is a soft,
-        silvery-white alkali metal. Under standard conditions, it is the lightest metal and the
-        lightest solid element.`,
-  },
-  {
-    position: 4,
-    name: 'Beryllium',
-    weight: 9.0122,
-    symbol: 'Be',
-    description: `Beryllium is a chemical element with symbol Be and atomic number 4. It is a
-        relatively rare element in the universe, usually occurring as a product of the spallation of
-        larger atomic nuclei that have collided with cosmic rays.`,
-  },
-  {
-    position: 5,
-    name: 'Boron',
-    weight: 10.811,
-    symbol: 'B',
-    description: `Boron is a chemical element with symbol B and atomic number 5. Produced entirely
-        by cosmic ray spallation and supernovae and not by stellar nucleosynthesis, it is a
-        low-abundance element in the Solar system and in the Earth's crust.`,
-  },
-  {
-    position: 6,
-    name: 'Carbon',
-    weight: 12.0107,
-    symbol: 'C',
-    description: `Carbon is a chemical element with symbol C and atomic number 6. It is nonmetallic
-        and tetravalent—making four electrons available to form covalent chemical bonds. It belongs
-        to group 14 of the periodic table.`,
-  },
-  {
-    position: 7,
-    name: 'Nitrogen',
-    weight: 14.0067,
-    symbol: 'N',
-    description: `Nitrogen is a chemical element with symbol N and atomic number 7. It was first
-        discovered and isolated by Scottish physician Daniel Rutherford in 1772.`,
-  },
-  {
-    position: 8,
-    name: 'Oxygen',
-    weight: 15.9994,
-    symbol: 'O',
-    description: `Oxygen is a chemical element with symbol O and atomic number 8. It is a member of
-         the chalcogen group on the periodic table, a highly reactive nonmetal, and an oxidizing
-         agent that readily forms oxides with most elements as well as with other compounds.`,
-  },
-  {
-    position: 9,
-    name: 'Fluorine',
-    weight: 18.9984,
-    symbol: 'F',
-    description: `Fluorine is a chemical element with symbol F and atomic number 9. It is the
-        lightest halogen and exists as a highly toxic pale yellow diatomic gas at standard
-        conditions.`,
-  },
-  {
-    position: 10,
-    name: 'Neon',
-    weight: 20.1797,
-    symbol: 'Ne',
-    description: `Neon is a chemical element with symbol Ne and atomic number 10. It is a noble gas.
-        Neon is a colorless, odorless, inert monatomic gas under standard conditions, with about
-        two-thirds the density of air.`,
-  },
+// Dados dos agrupamentos de monstros
+export const MONSTER_GROUPS: MonsterGroup[] = [
+  { set: 'Set 1', raceAndSize: 'Anjo/Médio', monsters: 'Angeling' },
+  { set: 'Set 2', raceAndSize: 'Inseto/Grande', monsters: 'Besouro-Ladrão Dourado, Maya' },
+  { set: 'Set 3', raceAndSize: 'Demônio/Médio', monsters: 'Deviling, Flor do Luar, Doppelganger' },
+  { set: 'Set 4', raceAndSize: 'Humanoide/Grande', monsters: 'Orc Herói, Senhor dos Orcs, Faraó' },
+  { set: 'Set 5', raceAndSize: 'Humanoide/Médio', monsters: 'Goblin, Jogadores, Sílfide' },
+  { set: 'Set 6', raceAndSize: 'Morto-Vivo/Médio', monsters: 'Drake, Osíris' },
+  { set: 'Set 7', raceAndSize: 'Bruto/Grande', monsters: 'Eddga, Freeoni, Quimera, Hatti, Wendine' },
+  { set: 'Set 8', raceAndSize: 'Inseto/Pequeno', monsters: 'Abelha-Rainha' },
+  { set: 'Set 9', raceAndSize: 'Demônio/Grande', monsters: 'Drácula, Bafomé, Barão Coruja, Lorde das Trevas, Gerente do Tempo' },
+  { set: 'Set 10', raceAndSize: 'Amorfo/Grande', monsters: 'Cavaleiro Sanguinário, Cavaleiro da Tempestade, Gnomo' },
+  { set: 'Set 11', raceAndSize: 'Planta/Pequeno', monsters: 'Ódio, Pesar' },
+  { set: 'Set 12', raceAndSize: 'Dragão/Grande', monsters: 'Dragão Mutante' },
+  { set: 'Set 13', raceAndSize: 'PeixePequeno/Médio', monsters: 'Maior General' },
+  { set: 'Set 14', raceAndSize: 'Amorfo/Médio', monsters: 'Salamandra' },
+  { set: 'Set 15', raceAndSize: 'Fantasma/Grande', monsters: 'Mestre Elementar' },
 ];
+
+// O total de monstros abrangidos pelos 15 sets é 34, conforme mencionado.
+
+
 
 @Component({
   selector: 'app-root',
@@ -154,7 +80,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
     MatTableModule,
     MatButtonModule,
     MatIconModule,
-    MatDivider
+    MatDivider,
+    MatInput
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [
@@ -172,7 +99,10 @@ export class AppComponent implements OnInit {
   dataSource: MVP[] = [];
   columnsToDisplay = ['name', 'race', 'element', 'size', 'event'];
   columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
-  expandedElement: PeriodicElement | null | undefined;
+  expandedElement: any | null | undefined;
+
+  displayedColumns: string[] = ['set', 'raceAndSize', 'monsters'];
+  dataSourceCardBelt = new MatTableDataSource<MonsterGroup>(MONSTER_GROUPS);
 
   tabLoadTimes: Date[] = [];
 
@@ -259,6 +189,12 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.getListMvps();
   }
+
+  applyFilterBelt(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourceCardBelt.filter = filterValue.trim().toLowerCase();
+  }
+
 
   getUrlCard(cardName: string, equipment: string) {
 
